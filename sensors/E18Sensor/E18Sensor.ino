30820be5f25b0095ecfc65b6c79c302208c161bb
NolanCampbell2357 <nolanc1223@gmail.com>
2022-02-03 19:28:57 -0600

    Update state

--- sensors/E18Sensor/E18Sensor.ino
+++ sensors/E18Sensor/E18Sensor.ino
@@ -1,29 +1,36 @@
 #include "JsonElement.h"
 #define E18Sensor 3
 
+JsonElement sensorFields[] = {
+    Json::Int("state", 0)};
+
+JsonElement devices[] = {
+    Json::Object("IRSensor", sensorFields)};
 
 JsonElement deviceFields[] = {
-  Json::Int("state", 0),
-  Json::String("deviceName", "E18Sensor")
-};
+    Json::String("name", "IntakeArduino"),
+    Json::Object("devices", devices)};
 
 JsonElement device = Json::Object(deviceFields);
 
-JsonState JState(device); 
+JsonState JState(device);
 
 int oldState = 3;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(E18Sensor, INPUT_PULLUP);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   int state = digitalRead(E18Sensor);
-  if(oldState != state){
-    JState.root() ["state"] = state;
+  if (oldState != state)
+  {
+    JState.root()["devices"]["IRSensor"]["state"] = state;
     JState.printJson(Serial);
     Serial.println("");
     oldState = state;
