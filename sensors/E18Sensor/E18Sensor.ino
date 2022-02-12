89dbe1289396a8db4acbe886e4d87f4bee248812
LlamaSlurper <LlamaSlurper@gmail.com>
2022-02-12 15:29:34 -0600

    Update E18Sensor.ino
    Made it a double sensor

--- sensors/E18Sensor/E18Sensor.ino
+++ sensors/E18Sensor/E18Sensor.ino
@@ -1,38 +1,57 @@
 #include "JsonElement.h"
-#define E18Sensor 3
+#define E18Sensor 2
+#define E18Sensor2 3
 
-JsonElement sensorFields[] = {
-    Json::Int("state", 0)};
+JsonElement intakeSensorFields[] = {
+    Json::Int("state",  1)};
+JsonElement feederSensorFields[] = {
+    Json::Int("state",  1)};
 
 JsonElement devices[] = {
-    Json::Object("IRSensor", sensorFields)};
+    Json::Object("intakeSensor" , intakeSensorFields),
+    Json::Object("feederSensor" , feederSensorFields)
+};
+
 
 JsonElement deviceFields[] = {
-    Json::String("name", "IntakeArduino"),
+    Json::String("name", "sensorsArduino"),
     Json::Object("devices", devices)};
 
 JsonElement device = Json::Object(deviceFields);
 
 JsonState JState(device);
 
-int oldState = 3;
+boolean isStateChanged = false;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(E18Sensor, INPUT_PULLUP);
+  pinMode(E18Sensor2, INPUT_PULLUP);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  int state = digitalRead(E18Sensor);
-  if (oldState != state)
-  {
-    JState.root()["devices"]["IRSensor"]["state"] = state;
+  int intakeState = digitalRead(E18Sensor);
+  int feederState = digitalRead(E18Sensor2);
+
+  if (JState.root()["devices"]["intakeSensor"]["state"].asInt() != intakeState){
+    JState.root()["devices"]["intakeSensor"]["state"] = intakeState;
+    isStateChanged = true;
+  }
+
+  if (JState.root()["devices"]["feederSensor"]["state"].asInt() != feederState){
+    JState.root()["devices"]["feederSensor"]["state"] = feederState;
+    isStateChanged = true;
+  }
+
+  if(isStateChanged == true){
     JState.printJson(Serial);
     Serial.println("");
-    oldState = state;
+    isStateChanged = false;
   }
+  
+  
 }
