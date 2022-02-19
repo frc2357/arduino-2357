3a2303c2f5cdb84f81acae22b3a1392e977d3cdc
NolanCampbell2357 <nolanc1223@gmail.com>
2022-02-19 13:27:38 -0600

    Change sensor name

--- sensors/E18Sensor/E18Sensor.ino
+++ sensors/E18Sensor/E18Sensor.ino
@@ -3,15 +3,13 @@
 #define E18Sensor2 3
 
 JsonElement intakeSensorFields[] = {
-    Json::Int("state",  1)};
+    Json::Int("state", 1)};
 JsonElement feederSensorFields[] = {
-    Json::Int("state",  1)};
+    Json::Int("state", 1)};
 
 JsonElement devices[] = {
-    Json::Object("intakeSensor" , intakeSensorFields),
-    Json::Object("feederSensor" , feederSensorFields)
-};
-
+    Json::Object("intakeIRSensor", intakeSensorFields),
+    Json::Object("feederIRSensor", feederSensorFields)};
 
 JsonElement deviceFields[] = {
     Json::String("name", "sensorsArduino"),
@@ -37,21 +35,22 @@ void loop()
   int intakeState = digitalRead(E18Sensor);
   int feederState = digitalRead(E18Sensor2);
 
-  if (JState.root()["devices"]["intakeSensor"]["state"].asInt() != intakeState){
-    JState.root()["devices"]["intakeSensor"]["state"] = intakeState;
+  if (JState.root()["devices"]["intakeIRSensor"]["state"].asInt() != intakeState)
+  {
+    JState.root()["devices"]["intakeIRSensor"]["state"] = intakeState;
     isStateChanged = true;
   }
 
-  if (JState.root()["devices"]["feederSensor"]["state"].asInt() != feederState){
-    JState.root()["devices"]["feederSensor"]["state"] = feederState;
+  if (JState.root()["devices"]["feederIRSensor"]["state"].asInt() != feederState)
+  {
+    JState.root()["devices"]["feederIRSensor"]["state"] = feederState;
     isStateChanged = true;
   }
 
-  if(isStateChanged == true){
+  if (isStateChanged == true)
+  {
     JState.printJson(Serial);
     Serial.println("");
     isStateChanged = false;
   }
-  
-  
 }
